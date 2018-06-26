50af7fbe08fb10a0b1ee5632aaf06d4865cdb401
Andrew Ching <fayf86@gmail.com>
2018-06-27 00:58:43 +0800

    Added a small patch to enable https support in the webservice.

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,9 @@ mkdir /creepMiner
 mv /creepMiner-src/bin/creepMiner /creepMiner/
 mv /creepMiner-src/resources/public /creepMiner/public
 
+# Make the web interface compatible with https
+sed -i 's/"ws:\/\/"/(location.protocol === "https:" ? "wss:\/\/" : "ws:\/\/")/g' creepMiner/public/js/general.js
+
 rm -r /creepMiner-src
 
 export SUDO_FORCE_REMOVE=yes
